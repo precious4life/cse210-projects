80bad5d95ec68d5860311cdd84f19753c8a2b05d
precious4life <preciousorugiengha@gmail.com>
2023-09-23 00:32:57 +0100

    added the prep3 work magic number

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first Name: ");
+
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //calling the random module
+        Random randomGen = new Random();
+
+        //setting the random number range
+        int magicNumber = randomGen.Next(1, 2);
+
+        //Console.WriteLine("Welcome to our mestries series, let see your brain level.");
+        //Console.Write("Guess a number from 1-100 ");
+
+        int guessNum = -1;
+        //guessNum = Convert.ToInt32(Console.ReadLine());
+
+        while (guessNum != magicNumber)
+        {   
+            Console.Write("What is your guess: ");
+            guessNum = int.Parse(Console.ReadLine());
+
+            if (guessNum < magicNumber){
+                Console.WriteLine("Guess highier next time");
+            }
+
+            else if (guessNum > magicNumber){
+                Console.WriteLine("guess lower next time");
+            }
+            else
+            {
+                Console.WriteLine("Welldone");
+                }
+        }
+
     }
 }
